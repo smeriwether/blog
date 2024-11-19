14a31c072004c86a3447f0d6bba93b3af2bbfd9c
Stephen Meriwether <stephen@merimerimeri.com>
2024-11-18 22:47:27 -0500

    Show the post datetimes relative to now and in eastern

--- blog/blog.py
+++ blog/blog.py
@@ -2,6 +2,8 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
+from datetime import datetime
+import pytz
 
 from blog.db import get_db
 
@@ -52,4 +54,36 @@ def nl2br(value):
         return value
     return value.replace('\n', '<br>')
 
+
+def format_datetime(value, include_relative=True):
+    eastern = pytz.timezone('US/Eastern')
+    if value.tzinfo is None:
+        value = pytz.utc.localize(value)
+    eastern_dt = value.astimezone(eastern)
+
+    # Get current time in Eastern
+    now = datetime.now(eastern)
+
+    # Calculate time difference
+    diff = now - eastern_dt
+
+    if include_relative:
+        if diff.days == 0:
+            if diff.seconds < 60:
+                return 'just now'
+            if diff.seconds < 3600:
+                minutes = diff.seconds // 60
+                return f'{minutes} minute{"s" if minutes != 1 else ""} ago'
+            if diff.seconds < 86400:
+                hours = diff.seconds // 3600
+                return f'{hours} hour{"s" if hours != 1 else ""} ago'
+        if diff.days == 1:
+            return 'Yesterday'
+        if diff.days < 7:
+            return f'{diff.days} days ago'
+
+    # Fall back to regular format for older dates
+    return eastern_dt.strftime('%B %d, %Y %I:%M %p %Z')
+
 bp.add_app_template_filter(nl2br, 'nl2br')
+bp.add_app_template_filter(format_datetime, 'format_datetime')
